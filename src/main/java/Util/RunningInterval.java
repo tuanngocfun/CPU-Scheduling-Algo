c0c534dd2588d9c899f32a9112fe5127733c0499
tuanngocfun <tuanngoccs50@gmail.com>
2024-05-10 17:55:59 +0700

    document with refactor

--- src/main/java/Util/RunningInterval.java
+++ src/main/java/Util/RunningInterval.java
@@ -1,69 +1,93 @@
-
 package Util;
 
-import Core.*;
+import Core.CycleComponent;
+import Core.Interval;
 import java.util.*;
 
 /**
- *
- * @version 1.0
+ * Manages intervals during which various cycle components are running.
  */
 public class RunningInterval {
-    private final Map<CycleComponent, Set<Interval>> mDataMap;
+    private final Map<CycleComponent, Set<Interval>> dataMap;
 
     public RunningInterval() {
-        mDataMap = new HashMap<>();
+        this.dataMap = new HashMap<>();
     }
-    
+
+    /**
+     * Adds a new interval for the given component.
+     *
+     * @param component The cycle component.
+     * @param startTime The start time of the interval.
+     * @param endTime The end time of the interval.
+     */
     public void add(CycleComponent component, int startTime, int endTime) {
         add(component, new Interval(startTime, endTime));
     }
-    
-    public void add(CycleComponent component, Interval interval) throws IllegalArgumentException {
-        if(component == null || interval == null)
-            throw new IllegalArgumentException();
-        
-        Set<Interval> intervalList = mDataMap.get(component);
-        if(intervalList == null) {
-            intervalList = new TreeSet<>();
-            intervalList.add(interval);
-            mDataMap.put(component, intervalList);
+
+    /**
+     * Adds the interval to the component's set of intervals.
+     *
+     * @param component The cycle component.
+     * @param interval The interval to add.
+     */
+    public void add(CycleComponent component, Interval interval) {
+        if (component == null || interval == null) {
+            throw new IllegalArgumentException("Component and interval must not be null.");
         }
-        else {
-            intervalList.add(interval);
-        }
+
+        Set<Interval> intervals = dataMap.computeIfAbsent(component, k -> new TreeSet<>());
+        intervals.add(interval);
     }
-    
+
+    /**
+     * Removes the specified interval from the component's set of intervals.
+     *
+     * @param component The cycle component.
+     * @param interval The interval to remove.
+     */
     public void remove(CycleComponent component, Interval interval) {
-        if(component == null || interval == null)
+        if (component == null || interval == null) {
             return;
-        
-        Set<Interval> intervalList = mDataMap.get(component);
-        if(intervalList != null) {
-            intervalList.remove(interval);
-            if(intervalList.isEmpty()) {
-                mDataMap.remove(component);
+        }
+
+        Set<Interval> intervals = dataMap.get(component);
+        if (intervals != null) {
+            intervals.remove(interval);
+            if (intervals.isEmpty()) {
+                dataMap.remove(component);
             }
         }
     }
-    
+
+    /**
+     * Returns all components.
+     *
+     * @return An array of all components.
+     */
     public CycleComponent[] getComponents() {
-        return mDataMap.keySet().toArray(CycleComponent[]::new);
+        return dataMap.keySet().toArray(new CycleComponent[0]);
     }
-    
+
+    /**
+     * Returns all intervals for all components.
+     *
+     * @return An array of all intervals.
+     */
     public Interval[] getIntervals() {
         List<Interval> resultList = new ArrayList<>();
-        mDataMap.values().forEach(set -> resultList.addAll(set));
-        return resultList.toArray(Interval[]::new);
+        dataMap.values().forEach(resultList::addAll);
+        return resultList.toArray(new Interval[0]);
     }
-    
+
+    /**
+     * Returns intervals for the specified component.
+     *
+     * @param component The cycle component.
+     * @return An array of intervals for the component.
+     */
     public Interval[] getIntervals(CycleComponent component) {
-        Set<Interval> intervalList = mDataMap.get(component);
-        if(intervalList != null) {
-            return intervalList.toArray(Interval[]::new);
-        }
-        else {
-            return null;
-        }
+        Set<Interval> intervals = dataMap.get(component);
+        return intervals != null ? intervals.toArray(new Interval[0]) : new Interval[0];
     }
 }

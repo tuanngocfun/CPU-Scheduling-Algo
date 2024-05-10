177c4da97f21b8084663d564809327486528dafe
tuanngocfun <tuanngoccs50@gmail.com>
2024-05-10 14:54:39 +0700

    refactor code

--- src/main/java/Util/CycleComponentNameComparator.java
+++ src/main/java/Util/CycleComponentNameComparator.java
@@ -14,22 +14,19 @@ public class CycleComponentNameComparator implements Comparator<String> {
     public int compare(String o1, String o2) {
         boolean isString1CPU = o1.contains("CPU");
         boolean isString2CPU = o2.contains("CPU");
-        if(isString1CPU && isString2CPU) {
+
+        if (isString1CPU && isString2CPU) {
             return o1.compareTo(o2);
-        }
-        else if(isString1CPU && !isString2CPU) {
+        } else if (isString1CPU) {
             return -1;
-        }
-        else if(!isString1CPU && isString2CPU) {
+        } else if (isString2CPU) {
             return 1;
-        }
-        else {
+        } else {
             try {
                 int ID1 = Integer.parseInt(o1.substring(1));
                 int ID2 = Integer.parseInt(o2.substring(1));
-                return Integer.valueOf(ID1).compareTo(ID2);
-            }
-            catch(NumberFormatException e) {
+                return Integer.compare(ID1, ID2);
+            } catch (NumberFormatException e) {
                 return o1.compareTo(o2);
             }
         }
